f1c3405eae4561fe34d579ec7e2065b6bb8207ee
ecunniet <ecunniet@e2r2p7.42.fr>
2017-01-10 18:14:46 +0100

    hehe

--- testligne2.c
+++ testligne2.c
@@ -6,7 +6,7 @@
 /*   By: ecunniet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/09 19:58:01 by ecunniet          #+#    #+#             */
-/*   Updated: 2017/01/09 20:44:29 by ecunniet         ###   ########.fr       */
+/*   Updated: 2017/01/10 18:14:23 by ecunniet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,60 +22,38 @@ void line(int xi, int yi, int xf, int yf, t_add *e)
 {
 	int dx;
 	int dy;
-	int cumul;
+	int err;
+	int e2;
 	int x;
 	int y;
 	int xinc;
 	int yinc;
 	int i;
 
-	i = 1;
 	x = xi;
 	y = yi;
-	dx = xf - xi;
-	dy = yf - yi;
-	xinc = (dx > 0) ? 1 : -1;
-	yinc = (dy > 0) ? 1 : -1;
-	dx = ABS(dx);
-	dy = ABS(dx);
+	dx = ABS(xf - xi);
+	dy = ABS(yf - yi);
+	xinc = (xf > yi) ? 1 : -1;
+	yinc = (yf > yi) ? 1 : -1;
+	err = ((dx < dy) ? dx : -dy)/2;
+	while (true)
+	{
 	*(e->adi + (x + y * 100) * 4) = 255;
 	*(e->adi + 1 + (x + y * 100) * 4) = 255;
 	*(e->adi + 2 + (x + y * 100) * 4) = 255;
-	if (dx < dy)
+	if (x == xf && y == yf)
+		return ;
+	e2 = err;
+	if (e2 > -dx)
 	{
-		cumul = dx / 2;
-		while (i <= dx)
-		{
-			x += xinc;
-			cumul += dy;
-			if (cumul <= dx)
-			{
-				cumul -= dx;
-				y += yinc;
-			}
-			*(e->adi + (x  + y * 100) * 4) = 255;
-			*(e->adi + 1 + (x + y * 100) * 4) = 255;
-			*(e->adi + 2 + (x + y * 100) * 4) = 255;
-			i++;
-		}
+		err -= dy;
+		x += xinc;
 	}
-	else
+	if (e2 < dy)
 	{
-		cumul = dy / 2;
-		while (i <= dy)
-		{
-			y += yinc;
-			cumul += dx;
-			if (cumul >= dy)
-			{
-				cumul -= dy;
-				x += xinc;
-			}
-			*(e->adi + (x  + y * 100) * 4) = 255;
-			*(e->adi + 1 + (x + y * 100) * 4) = 255;
-			*(e->adi + 2 + (x + y * 100) * 4) = 255;
-			i++;
-		}
+		err += dx;
+		y += yinc;
 	}
 }
 
@@ -97,7 +75,7 @@ int		ft_draw_pix()
 	win = mlx_new_window(mlx, 150, 150, "mlx_42");
 	img_ptr = mlx_new_image(mlx, width, height);
 	e.adi = mlx_get_data_addr(img_ptr, &bpp, &size_line, &endian);
-	line(50, 50, 10, 70, &e);
+	line(10, 100, 10, 70, &e);
 	mlx_put_image_to_window(mlx, win, img_ptr, 25, 25);
 	mlx_loop(mlx);
 	return (0);
