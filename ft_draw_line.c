da3cd9bfa38e8e196890da82fe7673007259fa04
ecunniet <ecunniet@e2r8p5.42.fr>
2017-01-12 16:47:32 +0100

    tracer ligne

--- ft_draw_line.c
+++ ft_draw_line.c
@@ -6,7 +6,7 @@
 /*   By: ecunniet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/09 19:58:01 by ecunniet          #+#    #+#             */
-/*   Updated: 2017/01/12 14:57:45 by ecunniet         ###   ########.fr       */
+/*   Updated: 2017/01/12 16:42:06 by ecunniet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,18 @@ typedef	struct	s_add
 	char	*adi;
 }				t_add;
 
+void	ft_light_pixel(int x, int y, t_add *e)
+{
+	*(e->adi + (x + y * 400) * 4) = 255;
+	*(e->adi + 1 + (x + y * 400) * 4) = 255;
+	*(e->adi + 2 + (x + y * 400) * 4) = 255;
+}
+
 void line(int xi, int yi, int xf, int yf, t_add *e)
 {
 	int dx;
 	int dy;
 	int err;
-	int e2;
 	int x;
 	int y;
 	int xinc;
@@ -36,24 +42,38 @@ void line(int xi, int yi, int xf, int yf, t_add *e)
 	dy = ABS(yf - yi);
 	xinc = (xf > xi) ? 1 : -1;
 	yinc = (yf > yi) ? 1 : -1;
-	err = ((dx < dy) ? dx : -dy)/2;
-	while (1)
+	ft_light_pixel(x, y, e);
+	i = 1;
+	if (dx > dy)
 	{
-		e->adi[(x + y * 100) * 4] = 255;
-		e->adi[1 + (x + y * 100) * 4] = 255;
-		e->adi[2 + (x + y * 100) * 4] = 255;
-		if (x == xf && y == yf)
-			return ;
-		e2 = err;
-		if (e2 > -dx)
+		err = dx / 2;
+		while (i <= dx)
 		{
-			err -= dy;
 			x += xinc;
+			err += dy;
+			if (err >= dx)
+			{
+				err -= dx;
+				y += yinc;
+			}
+			ft_light_pixel(x, y, e);
+			i++;
 		}
-		if (e2 < dy)
+	}
+	else
+	{
+		err = dy / 2;
+		while (i <= dy)
 		{
-			err += dx;
 			y += yinc;
+			err += dx;
+			if (err >= dy)
+			{
+				err -= dy;
+				x += xinc;
+			}
+			ft_light_pixel(x, y, e);
+			i++;
 		}
 	}
 }
@@ -69,27 +89,27 @@ int		ft_draw_pix()
 	int				size_line;
 	int				endian;
 	t_add			e;
-	int				xf;
-	int				yf;
+	int x;
+	int y;
 
-	width = 100;
-	height = 100;
+	width = 400;
+	height = 400;
 	mlx = mlx_init();
-	win = mlx_new_window(mlx, 150, 150, "mlx_42");
+	win = mlx_new_window(mlx, 500, 500, "mlx_42");
 	img_ptr = mlx_new_image(mlx, width, height);
 	e.adi = mlx_get_data_addr(img_ptr, &bpp, &size_line, &endian);
-	xf = 1;
-	while (xf < 100)
+	y = 0;
+	while (y < 400)
 	{
-		yf = 1;
-		while (yf < 100)
+		x = 0;
+		while (x < 400)
 		{
-			line(10, 10, xf, yf, &e);
-			yf++;
+		line(200, 200, x, y, &e);
+		x += 5;
 		}
-		xf++;
+		y += 5;
 	}
-	mlx_put_image_to_window(mlx, win, img_ptr, 25, 25);
+	mlx_put_image_to_window(mlx, win, img_ptr, 50, 50);
 	mlx_loop(mlx);
 	return (0);
 }
